fe0386891fe1c9cf2f673617be724b4935ef9df5
Gustavo-mts <gm357191@gmail.com>
2024-11-04 16:18:23 -0300

    feat: Inputs, selects e buttons

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,9 +1,28 @@
+import { Button } from '../../components/Button';
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function EditContact() {
   return (
-    <PageHeader
-      title="Editar Gustavo"
-    />
+    <>
+      <PageHeader
+        title="Editar Gustavo"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
+    </>
   );
 }
